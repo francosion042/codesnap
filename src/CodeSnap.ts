57e26fb1360ab1f5368602ba80a4c3141fe68b68
Anthony Nwobodo <anthonynwobodo7@gmail.com>
2024-02-08 21:06:35 +0100

    CodeSnap class

---
+++ src/CodeSnap.ts
@@ -0,0 +1,35 @@
+import puppeteer from "puppeteer";
+import sharp from "sharp";
+
+import {CodeSnapConfigType} from "./types";
+import {BackgroundColors, Themes} from "./enums";
+import {composeCarbonUrl} from "./utils";
+export default class CodeSnap {
+  private readonly theme: string
+  private readonly backgroundColor: string
+  private readonly numberLines: boolean
+  constructor(config: CodeSnapConfigType) {
+    this.theme = Themes[config.theme]
+    this.backgroundColor = BackgroundColors[config.backgroundColor]
+    this.numberLines = config.numberLines || false
+  }
+
+  public async snap (code: string) {
+    const carbonUrl = composeCarbonUrl(this.theme, this.backgroundColor, this.numberLines, code)
+    const browser = await puppeteer.launch({ headless: true, args: ["--no-sandbox", "--disable-setuid-sandbox"]});
+    const page = await browser.newPage();
+    await page.goto(carbonUrl);
+    const element = await page.$('.container-bg');
+
+    if (element) {
+      const buffer = await element.screenshot({ encoding: 'binary' });
+      await browser.close();
+      console.log(buffer)
+
+      await sharp(buffer).toFile(__dirname + 'output_image.png')
+    }
+  }
+
+}
+
+// new CodeSnap({theme: 'Monokai', backgroundColor: 'White',numberLines: true})
